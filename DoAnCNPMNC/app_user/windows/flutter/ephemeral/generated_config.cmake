7b94feebad6a847bc69c6fc10d3876d742506ea7
Trần Trọng Khang <23DH111538@st.huflit.edu.vn>
2025-10-28 22:54:25 +0700

    Xóa code cũ và cập nhật code mới của dự án

---
+++ DoAnCNPMNC/app_user/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\User\\AppData\\Local\\Flutter\\flutter_windows_3.22.2-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Workspace\\CNPM_nc\\Nhom15_QuanLyGiaoHang\\DoAnCNPMNC\\app_user" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\User\\AppData\\Local\\Flutter\\flutter_windows_3.22.2-stable\\flutter"
+  "PROJECT_DIR=C:\\Workspace\\CNPM_nc\\Nhom15_QuanLyGiaoHang\\DoAnCNPMNC\\app_user"
+  "FLUTTER_ROOT=C:\\Users\\User\\AppData\\Local\\Flutter\\flutter_windows_3.22.2-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Workspace\\CNPM_nc\\Nhom15_QuanLyGiaoHang\\DoAnCNPMNC\\app_user\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Workspace\\CNPM_nc\\Nhom15_QuanLyGiaoHang\\DoAnCNPMNC\\app_user"
+  "FLUTTER_TARGET=C:\\Workspace\\CNPM_nc\\Nhom15_QuanLyGiaoHang\\DoAnCNPMNC\\app_user\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMjIuMg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NzYxNzQ3YmZjNQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049YzI5ODA5MTM1MQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjI="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Workspace\\CNPM_nc\\Nhom15_QuanLyGiaoHang\\DoAnCNPMNC\\app_user\\.dart_tool\\package_config.json"
+)
